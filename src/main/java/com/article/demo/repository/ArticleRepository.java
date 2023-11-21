3d6f98e640a2b172358446569d93f4547ec1b152
Kim-KyuHyeok <kyuhyeok1011@naver.com>
2023-11-21 17:48:06 +0900

    추가 설정

--- src/main/java/com/article/demo/repository/ArticleRepository.java
+++ src/main/java/com/article/demo/repository/ArticleRepository.java
@@ -3,5 +3,6 @@ package com.article.demo.repository;
 import com.article.demo.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
