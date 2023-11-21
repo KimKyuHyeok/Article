18ecc8f3917465fa3b69c88190f157cc68fadaad
Kim-KyuHyeok <kyuhyeok1011@naver.com>
2023-11-21 17:14:48 +0900

    JPA Repository 기본 틀 작성

---
+++ src/main/java/com/article/demo/repository/ArticleRepository.java
@@ -0,0 +1,7 @@
+package com.article.demo.repository;
+
+import com.article.demo.domain.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArticleRepository extends JpaRepository<Article, Long> {
+}
