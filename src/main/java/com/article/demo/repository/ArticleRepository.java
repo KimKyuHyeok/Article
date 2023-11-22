02ab808eda1f0d261a28f2b37c90780b0e856265
Kim-KyuHyeok <kyuhyeok1011@naver.com>
2023-11-22 13:57:56 +0900

    Repository 수정

--- src/main/java/com/article/demo/repository/ArticleRepository.java
+++ src/main/java/com/article/demo/repository/ArticleRepository.java
@@ -2,6 +2,8 @@ package com.article.demo.repository;
 
 import com.article.demo.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
