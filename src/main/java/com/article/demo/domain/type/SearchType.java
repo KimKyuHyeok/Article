85026469089c83d2a8ff6421abba9bbb4e172bc4
Kim-KyuHyeok <kyuhyeok1011@naver.com>
2023-11-24 16:24:28 +0900

    게시글과 댓글 관련 DTO 를 작성하고 Test 코드를 작성함.
    일부 기능은 뼈대만 작성하였음.

---
+++ src/main/java/com/article/demo/domain/type/SearchType.java
@@ -0,0 +1,9 @@
+package com.article.demo.domain.type;
+
+public enum SearchType {
+    TITLE,
+    CONTENT,
+    ID,
+    NICKNAME,
+    HASHTAG
+}
---
+++ src/main/java/com/article/demo/service/ArticleService.java
@@ -0,0 +1,40 @@
+package com.article.demo.service;
+
+import com.article.demo.domain.type.SearchType;
+import com.article.demo.dto.ArticleDto;
+import com.article.demo.dto.ArticleUpdateDto;
+import com.article.demo.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class ArticleService {
+
+    private final ArticleRepository articleRepository;
+
+    @Transactional(readOnly = true)
+    public List<ArticleDto> searchArticles(SearchType searchType, String searchKeyword) {
+        return List.of();
+    }
+
+    @Transactional(readOnly = true)
+    public ArticleDto searchArticle(long l) {
+        return null;
+    }
+
+    public void saveArticle(ArticleDto dto) {
+
+    }
+
+    public void updateArticle(long articleId, ArticleUpdateDto dto) {
+    }
+
+    public void deleteArticle(long articleId) {
+    }
+}
