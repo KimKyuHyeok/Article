cb4504150c7ac2ad75877a7cf1f61a9dc876417a
Kim-KyuHyeok <kyuhyeok1011@naver.com>
2023-12-04 14:06:58 +0900

    pagination 기능 구현 및 테스트코드 작성

--- src/main/java/com/article/demo/controller/ArticleController.java
+++ src/main/java/com/article/demo/controller/ArticleController.java
@@ -5,7 +5,9 @@ import com.article.demo.dto.response.ArticleCommentResponse;
 import com.article.demo.dto.response.ArticleResponse;
 import com.article.demo.dto.response.ArticleWithCommentsResponse;
 import com.article.demo.service.ArticleService;
+import com.article.demo.service.PaginationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
@@ -31,6 +33,7 @@ import java.util.List;
 public class ArticleController {
 
     private final ArticleService articleService;
+    private final PaginationService paginationService;
 
     @GetMapping
     public String articles(
@@ -39,7 +42,12 @@ public class ArticleController {
             @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
             ModelMap map
     ) {
-        map.addAttribute("articles", articleService.searchArticles(searchType, searchValue, pageable).map(ArticleResponse::from));
+        Page<ArticleResponse> articles = articleService.searchArticles(searchType, searchValue, pageable).map(ArticleResponse::from);
+        List<Integer> barNumbers = paginationService.getPaginationBarNumbers(pageable.getPageNumber(), articles.getTotalPages());
+
+        map.addAttribute("articles", articles);
+        map.addAttribute("paginationBarNumbers", barNumbers);
+
         return "articles/index";
     }
 
--- src/test/java/com/article/demo/controller/MainControllerTest.java
+++ src/test/java/com/article/demo/controller/MainControllerTest.java
@@ -1,9 +1,12 @@
 package com.article.demo.controller;
 
 import com.article.demo.config.SecurityConfig;
+import com.article.demo.service.ArticleService;
+import com.article.demo.service.PaginationService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
